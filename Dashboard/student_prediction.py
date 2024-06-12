96766371cd6c8056dfb1d143d9120f2a83bed960
Ikhwanaminullah46 <143329729+Ikhwanaminullah46@users.noreply.github.com>
2024-06-12 23:14:06 +0700

    Update student_prediction.py

--- Dashboard/student_prediction.py
+++ Dashboard/student_prediction.py
@@ -151,7 +151,11 @@ with st.expander("View the Raw Data"):
 
 
 if st.button('Predict'):
-    new_data = data_preprocessing(data=data)
     with st.expander("View the Preprocessed Data"):
         st.dataframe(data=new_data, width=800, height=10)
     st.write("Student Status: {}".format(prediction(new_data)))
+
+st.write('Class explanation:')
+st.write('1. Dropout = Student failed to complete their study')
+st.write('2. Enrolled = Student is still studying and have not graduated yet')
+st.write('3. Graduate = Student has successfully finished their study')
